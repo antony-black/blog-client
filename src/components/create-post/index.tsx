f5c7f05477fec0ef13c22ec6dc87e268ebf79e06
Anton Knysh <anton.knysh@andevart.com>
2025-04-26 15:00:37 +0300

    created 'CreatePost'

---
+++ src/components/create-post/index.tsx
@@ -0,0 +1,68 @@
+import { Controller, useForm } from "react-hook-form";
+import {
+  TPostData,
+  useCreatePostMutation,
+  useLazyGetAllPostsQuery,
+} from "../../app/services/posts-api";
+import { Textarea } from "@nextui-org/react";
+import ErrorMessage from "../error-message";
+import CustomButton from "../custom-button";
+import { IoMdCreate } from "react-icons/io";
+
+export const CreatePost: React.FC = () => {
+  const [createPost] = useCreatePostMutation();
+  const [triggerGetAllPosts] = useLazyGetAllPostsQuery();
+  // const [error, setError] = useState<string>("");
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm<TPostData>();
+
+  const onSubmit = async (postData: TPostData) => {
+    try {
+      await createPost(postData).unwrap();
+      setValue("content", "");
+      await triggerGetAllPosts().unwrap();
+    } catch (error) {
+      // catchError(error);
+      console.error("error:", error);
+    }
+  };
+
+  const error = errors?.content?.message as string;
+
+  return (
+    <form className="flex-grow" onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="content"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: "Required field!",
+        }}
+        render={({ field }) => (
+          <Textarea
+            {...field}
+            labelPlacement="outside"
+            placeholder="What do you think?"
+            className="mb-5"
+          />
+        )}
+      />
+
+      {errors && <ErrorMessage error={error} />}
+
+      <CustomButton
+        className="flex-end"
+        color="success"
+        endContent={<IoMdCreate />}
+        type="submit"
+      >
+        Add post
+      </CustomButton>
+    </form>
+  );
+};
