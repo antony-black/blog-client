9c81ab27f41ceabe6a8e9275548b495ee97a8406
Anton Knysh <anton.knysh@andevart.com>
2025-04-26 15:01:30 +0300

    created 'User'

---
+++ src/components/user/index.tsx
@@ -0,0 +1,24 @@
+import { User as NextUiUser } from "@nextui-org/react";
+import { BASE_URL } from "../../constants";
+
+type TUser = {
+  name: string;
+  avatarUrl: string;
+  description?: string;
+  className?: string;
+};
+export const User: React.FC<TUser> = ({
+  name = "",
+  avatarUrl = "",
+  description = "",
+  className = "",
+}) => {
+  return (
+    <NextUiUser
+      className={className}
+      name={name}
+      description={description}
+      avatarProps={{src: `${BASE_URL}${avatarUrl}`}}
+    />
+  );
+};
