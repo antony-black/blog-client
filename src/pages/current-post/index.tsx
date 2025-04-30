765d70730b7486173fa4d536d614a3e3385e6a79
Anton Knysh <anton.knysh@andevart.com>
2025-04-30 08:55:38 +0300

    added "CurrentPost" page

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,7 +1,69 @@
+import { useParams } from "react-router-dom";
+import { useGetPostByIdQuery } from "../../app/services/posts-api";
+import { CustomCard } from "../../components/custom-card";
+import { GoBack } from "../../components/go-back";
+import { CreateComment } from "../../components/create-comment";
+
 const CurrentPost: React.FC = () => {
-  return <div>
-    CurrentPost
-  </div>;
+  const params = useParams<{ id: string }>();
+  const { data } = useGetPostByIdQuery(params.id ?? "");
+
+  if (!data) {
+    return <h2>There is no post!</h2>;
+  } else {
+    console.log('data >>>>', data);
+  }
+
+  // TODO: add DTO to the "author" properties to protect personal data: pass, createdAt, dateOfBirth, id etc.
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likes,
+    author,
+    likedByUser,
+    createdAt,
+  } = data;
+
+  return (
+    <>
+      <GoBack />
+      <CustomCard
+        id={id}
+        cardFor="current-post"
+        avatarUrl={author.avatarUrl ?? ""}
+        name={author.name ?? ""}
+        postAuthorId={authorId}
+        content={content}
+        likesCount={likes.length}
+        commentsCount={comments.length}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+      <div className="mt-10">
+        <CreateComment />
+      </div>
+
+      <div className="mt-10">
+        {data.comments
+          ? data.comments.map(comment => (
+              <CustomCard
+                key={comment.id}
+                id={id}
+                cardFor="comment"
+                avatarUrl={comment.user.avatarUrl ?? ""}
+                name={comment.user.name ?? ""}
+                postAuthorId={authorId}
+                comments={comments}
+                content={comment.content}
+                commentId={comment.id}
+              />
+            ))
+          : null}
+      </div>
+    </>
+  );
 };
 
 export default CurrentPost;
