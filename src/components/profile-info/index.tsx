04fed942ac4eb27d7db2d245982fc218a0683f2c
Anton Knysh <anton.knysh@andevart.com>
2025-05-01 11:24:06 +0300

    created the "ProfileInfo"

---
+++ src/components/profile-info/index.tsx
@@ -0,0 +1,11 @@
+type TProfileInfo = {
+  title: string;
+  info?: string;
+};
+
+export const ProfileInfo: React.FC<TProfileInfo>= ({title, info = "no info yet"}) => {
+  return <div className="font-semibold text-gray-500">
+    <span className="text-gray-200 mr-2">{title}</span>
+    {info}
+  </div>;
+};
