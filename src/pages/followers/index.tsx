b83beb1aa9218e81038389017e41d0da420e6587
Anton Knysh <anton.knysh@andevart.com>
2025-05-01 11:54:04 +0300

    added "Followers" & "Following" pages

--- src/pages/followers/index.tsx
+++ src/pages/followers/index.tsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+import { Card, CardBody } from "@nextui-org/react";
+
+import { useAppSelector } from "../../app/hooks";
+import { selectCurrent } from "../../features/auth-slice";
+
+import { User } from "../../components/user";
+
 const Followers: React.FC = () => {
-  return <div>
-    Followers
-    </div>;
+  const currentUser = useAppSelector(selectCurrent);
+
+  if (!currentUser?.followers || currentUser.followers.length <= 0) {
+    return <h2>You have no subscribers yet</h2>;
+  }
+
+  return (
+    <>
+      {currentUser.followers.map(user => (
+        <Link to={`/users/${user.follower.id}`} key={user.follower.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default Followers;
--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,7 +1,35 @@
-const Following: React.FC = () => {
-  return <div>
-    Following
-    </div>;
+import { Link } from "react-router-dom";
+import { Card, CardBody } from "@nextui-org/react";
+
+import { useAppSelector } from "../../app/hooks";
+import { selectCurrent } from "../../features/auth-slice";
+
+import { User } from "../../components/user";
+
+const Following : React.FC = () => {
+  const currentUser = useAppSelector(selectCurrent);
+
+  if (!currentUser?.following || currentUser.following.length <= 0) {
+    return <h2>You haven't had subscribed on any one yet.</h2>;
+  }
+
+  return (
+    <>
+      {currentUser.followers.map(user => (
+        <Link to={`/users/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.following.name ?? ""}
+                avatarUrl={user.following.avatarUrl ?? ""}
+                description={user.following.email}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </>
+  );
 };
 
-export default Following;
+export default Following ;
